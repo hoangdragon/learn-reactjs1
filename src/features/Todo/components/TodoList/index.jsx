0d895bbbe8ad1d10d6f1a64b8c0728db77fda654
hoangdragon <hoangpq114@gmail.com>
2023-02-19 20:43:41 +0700

    2

---
+++ src/features/Todo/components/TodoList/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss'
+
+TodoList.propTypes = {
+    todoList: PropTypes.array,
+};
+TodoList.defaultProps ={
+    todoList: [],
+}
+
+function TodoList({todoList}) {
+    return (
+        <ul className='test'>
+           {todoList.map(todo=>(
+                <li key = {todo.id}>{todo.title} </li>
+           ))} 
+        </ul>
+    );
+}
+
+export default TodoList;
